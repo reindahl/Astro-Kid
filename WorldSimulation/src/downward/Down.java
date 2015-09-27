6cceb02ec0190b993aa6486efe19da4b68bae755
Kasper Reindahl <Reindahl1@hotmail.com>
2015-09-27 14:28:39 +0200

    refactor + convinience

--- WorldSimulation/src/downward/Down.java
+++ WorldSimulation/src/downward/Down.java
@@ -1,6 +1,7 @@
 package downward;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -17,39 +18,116 @@ import world.World;
 
 public class Down {
 
-	public static Path downwardPath= Paths.get("/home/reindahl/downward/src/fast-downward.py");
-	
+	public static Path downwardPath = Paths.get("/home/reindahl/downward/src/fast-downward.py");
+
 	public static Path simpleDomain = Paths.get("levels/domain-simple.pddl");
+
+	public static Path domain = Paths.get("pddl/domain.pddl");
+	public static Path domain2 = Paths.get("pddl/domain2.pddl");
 	
-	public static void main(String[] args) throws InterruptedException,	IOException {
-		long startTime = System.currentTimeMillis();
-
-
-		generateSas(Paths.get("levels/domain2.pddl"), Paths.get("levels/prob04.pddl"));
+	public static void main(String[] args) throws InterruptedException, IOException {
+//		generatePDDL(Paths.get("levels"));
+		
+		runAllProblems(domain2);
+//		Path problemPath =Paths.get("pddl/level09.pddl");
+//		System.out.println(problemPath);
+//
+//		double totalTime;
+//		long startTime;
+//
+//		System.out.println("translate+preprocess");
+//		startTime = System.currentTimeMillis();
+//		generateSas(domain, problemPath);
+//		totalTime = (System.currentTimeMillis() - startTime) / 1000.;
+//		System.out.println(totalTime);
+//		
+//		System.out.println("search");
+//		startTime = System.currentTimeMillis();
 //		runSas(Paths.get("output"), true);
-//		run(Paths.get("levels/prob04.pddl"));
-//		Plan plan =run(Paths.get("levels/domain2.pddl"), Paths.get("levels/prob04v2.pddl"),true);
-//		System.out.println(plan);
-
-//		runAllProblems(Paths.get("levels/domain-simple2.pddl"),false);
-//		runAllProblems(Paths.get("levels/domain.pddl"),true);
-				
-		double totalTime =(System.currentTimeMillis()-startTime)/1000.;
-		System.out.println(totalTime);
+//		totalTime = (System.currentTimeMillis() - startTime) / 1000.;
+//		System.out.println(totalTime);
 	}
-
+	
 	private static ArrayList<String> filterList(ArrayList<String> out) {
-		ArrayList<String> filtered= new ArrayList<>();
-		boolean cut= true;
+		ArrayList<String> filtered = new ArrayList<>();
+		boolean cut = true;
 		for (String string : out) {
-			if(string.contains("Solution")){
-				cut=false;
+			if (string.contains("Solution")) {
+				cut = false;
 			}
-			if(!cut){
+			if (!cut) {
 				filtered.add(string);
 			}
 		}
 		return filtered;
+	}
+
+	public static void generatePDDL(Path path) throws FileNotFoundException {
+		if (Files.notExists(path)) {
+			throw new FileNotFoundException();
+		}
+		if (Files.isDirectory(path)) {
+
+			try {
+				Files.newDirectoryStream(path).forEach(e -> {
+					try {
+						generatePDDL(e);
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				});
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else if (path.toString().endsWith(".xml")) {
+
+			World world = new World(path);
+			String name = path.getFileName().toString();
+			name = name.substring(0, name.length() - 4);
+			name += ".pddl";
+			try {
+				Converter.toPDDL(world, Paths.get("pddl", name));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	public static Path generateSas(Path domainPath, Path problemPath) throws IOException, InterruptedException {
+		// --translate --preprocess
+		ProcessBuilder pb;
+		if (domainPath != null) {
+			pb = new ProcessBuilder(downwardPath.toString(), "--translate", "--preprocess", domainPath.toString(),
+					problemPath.toString());
+		} else {
+			pb = new ProcessBuilder(downwardPath.toString(), "--translate", "--preprocess", problemPath.toString());
+		}
+		long startTime = System.currentTimeMillis();
+		Process process = pb.start();
+		int errCode = process.waitFor();
+		double totalTime = (System.currentTimeMillis() - startTime) / 1000.;
+		// System.out.println("downward executed, any errors? " + (errCode == 0
+		// ? "No" : "Yes"));
+
+		if (errCode == 0) {
+			ArrayList<String> out = outputToList(process.getInputStream());
+			out.add("TotalTime: " + totalTime + "s");
+			String outputName = problemPath.toString();
+			outputName = outputName.substring(0, outputName.length() - 4) + "outputSas";
+			Files.write(Paths.get(outputName), out, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING,
+					StandardOpenOption.CREATE);
+
+			return Paths.get("output");
+		} else {
+
+			System.out.println("downward eroor Output:\n" + output(process.getErrorStream()));
+			return null;
+		}
 	}
 
 
@@ -68,7 +146,6 @@ public class Down {
 		return sb.toString();
 	}
 
-
 	private static ArrayList<String> outputToList(InputStream inputStream) throws IOException {
 		ArrayList<String> list = new ArrayList<>();
 		BufferedReader br = null;
@@ -84,48 +161,51 @@ public class Down {
 		return list;
 	}
 
-	public static Plan run(Path domain, Path problemPath) throws IOException, InterruptedException {
-		return run(domain, problemPath, false);
-	}
-
-	public static Plan run(Path problemPath) throws IOException, InterruptedException{
+	public static Plan run(Path problemPath) throws IOException, InterruptedException {
 		return run(null, problemPath, false);
 	}
 
 	public static Plan run(Path problemPath, boolean output) throws IOException, InterruptedException {
 		return run(null, problemPath, output);
 	}
-	public static Plan run(Path domainPath, Path problemPath, Boolean output) throws IOException, InterruptedException{
-		//./fast-downward.py ../pddl/prob01.pddl --search "astar(blind())"
-		//./fast-downward.py ../pddl/level4v2.pddl --search "astar(ff())"
+
+	public static Plan run(Path domain, Path problemPath) throws IOException, InterruptedException {
+		return run(domain, problemPath, false);
+	}
+
+	public static Plan run(Path domainPath, Path problemPath, Boolean output) throws IOException, InterruptedException {
+		// ./fast-downward.py ../pddl/prob01.pddl --search "astar(blind())"
+		// ./fast-downward.py ../pddl/level4v2.pddl --search "astar(ff())"
 		ProcessBuilder pb;
-		if(domainPath!= null){
-			pb = new ProcessBuilder(downwardPath.toString(), domainPath.toString(), problemPath.toString(), "--search", "astar(ff())");
-		}else{
+		if (domainPath != null) {
+			pb = new ProcessBuilder(downwardPath.toString(), domainPath.toString(), problemPath.toString(), "--search",
+					"astar(ff())");
+		} else {
 			pb = new ProcessBuilder(downwardPath.toString(), problemPath.toString(), "--search", "astar(ff())");
 		}
 		long startTime = System.currentTimeMillis();
 		Process process = pb.start();
 		int errCode = process.waitFor();
-		double totalTime =(System.currentTimeMillis()-startTime)/1000.;
-		//		System.out.println("downward executed, any errors? " + (errCode == 0 ? "No" : "Yes"));
+		double totalTime = (System.currentTimeMillis() - startTime) / 1000.;
+		// System.out.println("downward executed, any errors? " + (errCode == 0
+		// ? "No" : "Yes"));
 
-
-		if(errCode==0){
-			ArrayList<String> out =outputToList(process.getInputStream());
-			out.add("TotalTime: "+totalTime+"s");
-			if(output){
+		if (errCode == 0) {
+			ArrayList<String> out = outputToList(process.getInputStream());
+			out.add("TotalTime: " + totalTime + "s");
+			if (output) {
 				String outputName = problemPath.toString();
-				outputName=outputName.substring(0, outputName.length()-4)+"output";
-				Files.write(Paths.get(outputName), out, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+				outputName = outputName.substring(0, outputName.length() - 4) + "output";
+				Files.write(Paths.get(outputName), out, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING,
+						StandardOpenOption.CREATE);
 			}
-			out=filterList(out);
-			
+			out = filterList(out);
+
 			return new Plan(out, totalTime);
-		}else{
-			
+		} else {
+
 			System.out.println("downward eroor Output:\n" + output(process.getErrorStream()));
-			return null;
+			return new Plan(totalTime);
 		}
 	}
 
@@ -135,49 +215,63 @@ public class Down {
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
-	public static Plan run(String name) throws IOException, InterruptedException{
+	public static Plan run(String name) throws IOException, InterruptedException {
 		Path path = Paths.get(name);
-		if(Files.exists(path)){
+		if (Files.exists(path)) {
 			return run(path);
-		}else{
-			return run(Paths.get("/home/reindahl/downward/pddl/"+name));
+		} else {
+			return run(Paths.get("/home/reindahl/downward/pddl/" + name));
 
 		}
 	}
+	
+	public static void runAllProblems() {
 
-	public static void runAllProblems(Path domain, Boolean output){
-		Path levels= Paths.get("levels/");
+		runAllProblems(null, false);
+	}
+
+	public static void runAllProblems(Boolean output) {
+
+		runAllProblems(null, output);
+	}	
+	public static void runAllProblems(Path domain) {
+
+		runAllProblems(domain, false);
+	}
+	public static void runAllProblems(Path domain, Boolean output) {
+		Path levels = Paths.get("levels/");
 
 		try {
 			for (Path path : Files.newDirectoryStream(levels)) {
-				if(path.getFileName().toString().matches("^prob\\d{2}(v\\d)?\\.xml$")){
+				if (path.getFileName().toString().matches("^prob\\d{2}(v\\d)?\\.xml$")) {
 					try {
 						System.out.println(path);
-						World world=new World(path);
-						ArrayList<String> pddl = Converter.toPDDL(world, path.getFileName().toString().substring(0, path.getFileName().toString().length()-4));
+						World world = new World(path);
+						ArrayList<String> pddl = Converter.toPDDL(world,
+								path.getFileName().toString().substring(0, path.getFileName().toString().length() - 4));
 						String outputName = path.toString();
-						outputName=outputName.substring(0, outputName.length()-3)+"pddl";
-						Files.write(Paths.get(outputName), pddl, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+						outputName = outputName.substring(0, outputName.length() - 3) + "pddl";
+						Files.write(Paths.get(outputName), pddl, StandardCharsets.UTF_8,
+								StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
 
-						Plan plan=run(Paths.get(outputName), output);
-						if(domain!= null && Files.exists(domain)){
-							plan=run(domain,Paths.get(outputName), output);
-						}else{
-							plan=run(Paths.get(outputName), output);
+						Plan plan;
+						if (domain != null && Files.exists(domain)) {
+							plan = run(domain, Paths.get(outputName), output);
+						} else {
+							plan = run(Paths.get(outputName), output);
 						}
-						if(plan==null){
+						if (plan == null) {
 							System.out.println("downward errors");
-						}else if(plan.getCommands().isEmpty()){
+						} else if (plan.getCommands().isEmpty()) {
 							System.out.println("failed to find solution");
-						}else{
-							System.out.println("found solution in "+plan.getTotalTime());
+						} else {
+							System.out.println("found solution in " + plan.getTotalTime());
 						}
 						System.out.println();
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-
 
 				}
 
@@ -187,66 +281,67 @@ public class Down {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void runAllProblems(Boolean output){
 
-		runAllProblems(null, output);
-	}
-	public static Path generateSas(Path domainPath, Path problemPath) throws IOException, InterruptedException{
-		// --translate --preprocess 
+	public static Plan runSas(Path sasPath, Boolean output) throws IOException, InterruptedException {
+
 		ProcessBuilder pb;
-		if(domainPath!= null){
-			pb = new ProcessBuilder(downwardPath.toString(), "--translate", "--preprocess", domainPath.toString(), problemPath.toString());
-		}else{
-			pb = new ProcessBuilder(downwardPath.toString(), "--translate", "--preprocess", problemPath.toString());
-		}
+
+		pb = new ProcessBuilder(downwardPath.toString(),
+				"/media/reindahl/Backup/GitHub/Astro-Kid/WorldSimulation/" + sasPath.toString(), "--search",
+				"astar(ff())");
 		long startTime = System.currentTimeMillis();
 		Process process = pb.start();
 		int errCode = process.waitFor();
-		double totalTime =(System.currentTimeMillis()-startTime)/1000.;
-		//		System.out.println("downward executed, any errors? " + (errCode == 0 ? "No" : "Yes"));
+		double totalTime = (System.currentTimeMillis() - startTime) / 1000.;
+		// System.out.println("downward executed, any errors? " + (errCode == 0
+		// ? "No" : "Yes"));
 
+		if (errCode == 0) {
+			ArrayList<String> out = outputToList(process.getInputStream());
+			out.add("TotalTime: " + totalTime + "s");
+			if (output) {
+				String outputName = sasPath.toString();
+				outputName = outputName.substring(0, outputName.length()) + ".Search";
+				Files.write(Paths.get(outputName), out, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING,
+						StandardOpenOption.CREATE);
+			}
+			out = filterList(out);
 
-		if(errCode==0){
-			ArrayList<String> out =outputToList(process.getInputStream());
-			out.add("TotalTime: "+totalTime+"s");
-				String outputName = problemPath.toString();
-				outputName=outputName.substring(0, outputName.length()-4)+"outputSas";
-				Files.write(Paths.get(outputName), out, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
-
-			return null;
-		}else{
-			
+			return new Plan(out, totalTime);
+		} else {
+			outputToList(process.getInputStream()).forEach(e -> System.out.println(e.toString()));
 			System.out.println("downward eroor Output:\n" + output(process.getErrorStream()));
 			return null;
 		}
 	}
-	
-	public static Plan runSas(Path sasPath, Boolean output) throws IOException, InterruptedException{
 
+	public static Path translate(Path domainPath, Path problemPath) throws IOException, InterruptedException {
+		// --translate --preprocess
 		ProcessBuilder pb;
-
-		pb = new ProcessBuilder(downwardPath.toString(), "/media/reindahl/Backup/GitHub/Astro-Kid/WorldSimulation/"+sasPath.toString(), "--search", "astar(ff())");
+		if (domainPath != null) {
+			pb = new ProcessBuilder(downwardPath.toString(), "--translate", domainPath.toString(),
+					problemPath.toString());
+		} else {
+			pb = new ProcessBuilder(downwardPath.toString(), "--translate", problemPath.toString());
+		}
 		long startTime = System.currentTimeMillis();
 		Process process = pb.start();
 		int errCode = process.waitFor();
-		double totalTime =(System.currentTimeMillis()-startTime)/1000.;
-		//		System.out.println("downward executed, any errors? " + (errCode == 0 ? "No" : "Yes"));
+		double totalTime = (System.currentTimeMillis() - startTime) / 1000.;
+		// System.out.println("downward executed, any errors? " + (errCode == 0
+		// ? "No" : "Yes"));
 
-		
-		if(errCode==0){
-			ArrayList<String> out =outputToList(process.getInputStream());
-			out.add("TotalTime: "+totalTime+"s");
-			if(output){
-				String outputName = sasPath.toString();
-				outputName=outputName.substring(0, outputName.length()-3)+"output";
-				Files.write(Paths.get(outputName), out, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
-			}
-			out=filterList(out);
-			
-			return new Plan(out, totalTime);
-		}else{
-			outputToList(process.getInputStream()).forEach(e -> System.out.println(e.toString()));
+		if (errCode == 0) {
+			ArrayList<String> out = outputToList(process.getInputStream());
+			out.add("TotalTime: " + totalTime + "s");
+			String outputName = problemPath.toString();
+			outputName = outputName.substring(0, outputName.length() - 4) + "outputTranslate";
+			Files.write(Paths.get(outputName), out, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING,
+					StandardOpenOption.CREATE);
+
+			return null;
+		} else {
+
 			System.out.println("downward eroor Output:\n" + output(process.getErrorStream()));
 			return null;
 		}
